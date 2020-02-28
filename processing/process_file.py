bd96c6e236bccb4544ecb20d406b67c0932ffa0f
Chris Wieringa <chris@wieringafamily.com>
2020-02-28 18:27:11 -0500

    processing/* - initial functions testimages/* - test images testing/* - test script area

---
+++ processing/process_file.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+# process_file.py
+# Chris Wieringa, cwiering@umich.edu, 2020-02-28
+# Michael Farmer, CSC548 Winter 2020 UM Flint
+#
+# Purpose:  load an image file, then run a variety of different 
+#   person detection routines on it to get a count of users
+#
+# Input:  
+#   --image <filename> - which image file will be loaded
+#   --outimages <boolean True or False> - outputs processed images per algorithm
+#   --scale <boolean True or False> - scale the input image
+#   --scalepercent <int 0-100> - percent to scale the image to
+#   --showprocessed <boolean True or False> - show processed images per algorithm
+# Output:
+#   JSON data, algorithmname with number of persons detected (people?)
+
+# # # # # #
+# SETUP: Start
+
+# imports
+import numpy as np
+import argparse
+import cv2
+import json
+from pathlib import Path,PurePath
+
+# import my processing classes
+from mobileNetSSD import mobileNetSSD
+
+# parse my arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("--image",required=True,
+        help="image filepath")
+ap.add_argument("--outimages", type=bool, default=False,
+        help="boolean switch to output images")
+ap.add_argument("--scale", type=bool, default=False,
+        help="boolean switch to scale images")
+ap.add_argument("--scalepercent", type=int, default=50,
+        help="percent to scale image to, must set --scale")
+ap.add_argument("--show", type=bool, default=False,
+        help="boolean switch to show processed images")
+args = vars(ap.parse_args())
+
+# output var
+output = {}
+outimages = {}
+
+# other variables
+imagefile = PurePath(args['image'])
+basefilename = imagefile.stem
+outimagebasefilename = "outputs/{}".format(basefilename)
+outimagestatsjson = "outputs/{}.json".format(basefilename)
+
+# SETUP: End
+# # # # # #
+
+# # # # # #
+# MAIN Program: Start
+
+# first off, open the file and scale if required
+image = cv2.imread(args["image"])
+if(args["scale"]):
+    new_width =  int(image.shape[1] * args["scalepercent"] / 100)
+    new_height = int(image.shape[0] * args["scalepercent"] / 100) 
+    image = cv2.resize(image, (new_width, new_height))
+(height,width) = image.shape[:2]
+dimen = (width,height)
+
+# next, pass it off to all of the algorithms for testing
+
+# 1. mobilenet_ssd neural network detection
+# based off of https://www.pyimagesearch.com/2017/09/11/object-detection-with-deep-learning-and-opencv/ and https://github.com/chuanqi305/MobileNet-SSD.  This is my base-case "commercial" detection
+# algorithm to compare my performance off of
+conf = 0.25
+proc1 = mobileNetSSD(image,conf,args["show"])
+proc1.process()
+output["mobileNetSSD"] = proc1.numpersons
+outimages["mobileNetSSD"] = proc1.image
+
+# Pause to display images
+if args["show"]:
+    cv2.waitKey(0)
+
+
+# Outputs section
+# If necessary, output the images to the outputs directory
+if args["outimages"]:
+    for key in outimages:
+        outfilename = "{}-{}.jpg".format(outimagebasefilename,key)
+        cv2.imwrite(outfilename,outimages[key])
+
+# Output the output variable to JSON STDOUT and to file
+print(json.dumps(output))
+with open(outimagestatsjson,'w+') as outjsonfile:
+    json.dump(output, outjsonfile)
+
+# MAIN Program: End
+# # # # # #
+
